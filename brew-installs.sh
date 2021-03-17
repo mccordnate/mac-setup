9dbfbd886d911151dcbea6865b66d771ac4b9bc7
Nate McCord <nate@Nates-MBP.localdomain>
2021-03-17 13:26:47 -0700

    Added work & personal options as well as rectangle config

--- brew-installs.sh
+++ brew-installs.sh
@@ -15,10 +15,15 @@ brew upgrade
 
 BREW_PREFIX=$(brew --prefix)
 
+
+#Ask if for work or personal setup
+read -p "Is this for [work/personal] setup? " setuptype
+
+# Installs for work and personal
+ 
 brew tap railwaycat/emacsmacport
 
-brew install emacs-mac
-brew install emacs
+brew install --cask emacs-mac
 brew install git
 brew install mas
 brew install jq
@@ -26,7 +31,6 @@ brew install tmux
 brew install wget
 brew install tree
 brew install coreutils
-brew install nmap
 brew install sqlite
 brew install p7zip
 brew install docker-compose
@@ -40,46 +44,63 @@ brew install trash
 brew install thefuck
 brew install the_silver_searcher
 brew install gnupg
+brew install z
+brew install karabiner-elements
+brew install rectangle
 
+if [ $setuptype == "work" ]
+then
+  brew install wireguard-tools
 
-brew install node
-brew install nvm
-brew install pyenv
-brew install yarn
-brew install python3
-brew install docker
-brew install rustup
+if [ $setuptype == "personal" ]
+then
+  brew install nmap
+  brew install node
+  brew install nvm
+  brew install pyenv
+  brew install yarn
+  brew install python3
+  brew install docker
+  brew install rustup
+fi
+
+# Brew casks for both work and personal
 
 brew install --cask nextcloud
+brew install --cask fantastical
+brew install --cask signal
 brew install --cask google-chrome
 brew install --cask iterm2
 brew install --cask spotify
 brew install --cask vlc
 brew install --cask visual-studio-code
 brew install --cask zoom
-brew install --cask discord
 brew install --cask slack
 brew install --cask little-snitch
 brew install --cask micro-snitch
 brew install --cask 1password
-brew install --cask figma
-brew install --cask gimp
 brew install --cask alfred
 brew install --cask maccy
 brew install --cask firefox
 brew install --cask java
 brew install --cask insomnia
-brew install --cask android-studio
-brew install --cask ipfs
-brew install --cask ledger-live
-brew install --cask messenger
-brew install --cask microsoft-office
-brew install --cask protonvpn
-brew install --cask telegram
-brew install --cask standard-notes
 brew install --cask swish
-brew install --cask rocket
 brew install --cask vanilla
 brew install --cask bettertouchtool
+brew install --cask telegram
+
+if [ $setuptype == "personal" ]
+then
+  brew install --cask ipfs
+  brew install --cask ledger-live
+  brew install --cask microsoft-office
+  brew install --cask protonvpn
+  brew install --cask standard-notes
+  brew install --cask figma
+  brew install --cask gimp
+  brew install --cask android-studio
+  brew install --cask messenger
+  brew install --cask discord
+fi
 
 brew cleanup
